748078b0947118111a2f28f2d52a969f395fe070
Yoan Sallami <ysallami@laas.fr>
2020-01-06 16:07:22 +0100

    update install script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,2 +1,3 @@
 pip install -r requirements.txt --user
-echo "#To override ROS opencv-python package with newer version:\\r\\nexport PYTHONPATH='\${HOME}/.local/lib/python2.7/site-packages'" >> ~/.bashrc
+echo "#To override ROS opencv-python package with newer version:" >> ~/.bashrc
+echo "export PYTHONPATH='\${HOME}/.local/lib/python2.7/site-packages'" >> ~/.bashrc
