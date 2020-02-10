c8662ddbb22c6ba58e3e29ca531daa2745298304
Yoan Sallami <ysallami@pepperadd>
2020-02-10 13:59:20 +0100

    modify dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,3 +1,4 @@
 pip install -r requirements.txt --user
+sudo apt install ros-${ROS_DISTRO}-joint-state-publisher-gui
 echo "#To override ROS opencv-python package with newer version:" >> ~/.bashrc
 echo "export PYTHONPATH='\${HOME}/.local/lib/python2.7/site-packages'" >> ~/.bashrc
